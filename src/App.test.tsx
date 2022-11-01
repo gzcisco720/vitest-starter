ded04ae8364acc5be6d6503f6939367befe03fe3
Eric <gzcisco720@gmail.com>
2022-11-01 21:25:47 +1100

    feat(init): first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('should render component', () => {
+    render(<App />);
+    const titleElement = screen.getByText('src/App.tsx');
+    expect(titleElement).toBeInTheDocument();
+  });
+});
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.config.ts',
+  },
+  plugins: [react()],
+});
